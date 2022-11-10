416d91098c8afa093c44e977e4e3e4f67c7358e1
Danny-M11 <meddane22@gmail.com>
2022-11-10 13:31:56 +0300

    созданы модули

---
+++ js/utils.js
@@ -0,0 +1,25 @@
+//программа вывода случайного числа
+const randomInteger = (a, b) => {
+  if (a < 0 || b < 0) {
+    return NaN;
+  }
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+//программа проверки длины строки
+const checkStringLimit = (string, limit) => {
+  const length = string.length;
+
+  if (length > limit) {
+    return 'превышен допустимый лимит строки';
+  }
+
+  return length;
+};
+
+checkStringLimit('Word', 11);
+
+export {randomInteger};
