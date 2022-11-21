941b7515066f18784c6802bd4b7dd7b799ab4665
Danny-M11 <meddane22@gmail.com>
2022-11-21 21:43:15 +0300

    исправил валидацию формы

--- js/formValidation.js
+++ js/formValidation.js
@@ -1,7 +1,16 @@
 const form = document.querySelector('.img-upload__form');
 
-new Pristine(form, {
+const pristine = new Pristine(form, {
   classTo: 'img-upload__text',
   errorTextParent: 'img-upload__text',
   errorTextClass: 'text-error',
+});
+
+
+form.addEventListener('submit', (evt) => {
+  const isValid = pristine.validate();
+
+  if (!isValid) {
+    evt.preventDefault();
+  }
 });
